7cd17daec43c878d78b2033024f4c46fe2dd4ebd
rlzijdeman <richard.zijdeman@gmail.com>
2014-10-29 09:07:03 +0100

    retrieve countries with 2 or more years

--- R/more_than_two_years01.R
+++ R/more_than_two_years01.R
@@ -14,3 +14,17 @@ load("./data/git_combine_data.Rda") # loads labrel.df into memory
 
 # preserve data from labrel.df with more than 1 observation
 all.dt <- data.table(labrel.df)
+all.dt[, year.count := length(unique(bmyear)), by = country]
+table(all.dt$year.count)
+
+all.dt.2 <- all.dt[year.count >1, ]
+all.dt.3 <- all.dt[year.count >2, ]
+all.dt.4 <- all.dt[year.count >3, ]
+
+write.csv(all.dt.2, file = "./data/git_combine_data_2yearsplus.csv")
+write.csv(all.dt.3, file = "./data/git_combine_data_3yearsplus.csv")
+write.csv(all.dt.4, file = "./data/git_combine_data_4yearsplus.csv")
+
+# EOF
+
+
